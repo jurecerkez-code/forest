28692cadca527b19a32c8996e69332fce58d40e9
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-05 15:01:12 +0200

    ?

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -3,15 +3,18 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Trip;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
 class CommentFactory extends Factory
 {
     public function definition(): array
     {
         return [
-            'trip_id' => Trip::factory(), // each comment belongs to a trip
-            'comment' => fake()->sentence(),
+            'trip_id' => \App\Models\Trip::factory(),   // create a trip automatically
+            'user_id' => \App\Models\User::factory(),   // create a user automatically
+            'comment' => fake()->sentence(),            // random comment
         ];
     }
 }
