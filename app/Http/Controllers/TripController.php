55d4d03e0537ad5137821ae732f0ed7982176911
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-05 12:44:55 +0200

    tried to make index

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class TripController extends Controller
 {
-    
+    public function index()
+    {
+        // Load trips with their user and comments
+        $trips = Trip::with('user', 'comments')->get();
+
+        // Send data to the view
+        return view('trips.index', compact('trips'));
+    }
 }
