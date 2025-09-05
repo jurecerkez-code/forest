623997de063985e6012b9a325ab4d236a11d2234
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-05 16:47:00 +0200

    tripfactory update

--- database/factories/TripFactory.php
+++ database/factories/TripFactory.php
@@ -1,11 +1,26 @@
-// database/factories/TripFactory.php
-public function definition(): array
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Trip;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Trip>
+ */
+class TripFactory extends Factory
 {
-    return [
-        'title' => $this->faker->sentence(3),
-        'description' => $this->faker->paragraph(),
-        'start_date' => $this->faker->date(),
-        'end_date' => $this->faker->date(),
-        'user_id' => 1,
-    ];
+    protected $model = Trip::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => 1,
+            'start_time' => $this->faker->dateTimeBetween('-1 month', 'now'),
+            'end_time' => $this->faker->dateTimeBetween('now', '+1 month'),
+            'duration' => $this->faker->numberBetween(10, 300),
+            'voice_recording' => $this->faker->optional()->url(),
+            'satisfaction' => $this->faker->numberBetween(1, 5),
+        ];
+    }
 }
