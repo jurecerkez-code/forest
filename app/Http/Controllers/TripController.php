1cd044a76d409018b64e148c6ab47daae7990634
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-05 12:41:43 +0200

    created index within controller

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class TripController extends Controller
 {
-    //
+    public function index()
+    {
+        $trips = Trip::with('user', 'comments')->get();
+        return view('trips.index', compact('trips'));
+    }
+
+    public function show($id)
+    {
+        $trip = Trip::with('user', 'comments.user')->findOrFail($id);
+        return view('trips.show', compact('trip'));
+    }
 }
