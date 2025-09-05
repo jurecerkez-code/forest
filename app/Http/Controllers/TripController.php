7a361caed53f86552dd67d8a5e79d953c7bde030
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-05 12:43:33 +0200

    returned to previous step

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -6,15 +6,5 @@ use Illuminate\Http\Request;
 
 class TripController extends Controller
 {
-    public function index()
-    {
-        $trips = Trip::with('user', 'comments')->get();
-        return view('trips.index', compact('trips'));
-    }
-
-    public function show($id)
-    {
-        $trip = Trip::with('user', 'comments.user')->findOrFail($id);
-        return view('trips.show', compact('trip'));
-    }
+    
 }
