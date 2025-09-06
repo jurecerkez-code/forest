5c6648fff1fd6b0012f37e7295e8961d31647e2e
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-06 20:55:03 +0200

    deleted layouts file and created folder, inside is file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forest App</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="{{ route('home') }}">Home</a> |
+            <a href="{{ route('trips.index') }}">Trips</a>
+        </nav>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
